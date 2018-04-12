228a04d7ffb7332c376daa0f19cb07ad4298baf9
Louis-marie JOLY <ljoly@e2r11p1.42.fr>
2018-04-12 17:02:36 +0200

    client/add socket connection

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import TileSection from './tiles/TileSection.jsx'
-import socketIOClient from 'socket.io-client'
+import io from 'socket.io-client'
 
 class App extends Component{
     constructor(props){
@@ -12,24 +12,11 @@ class App extends Component{
                     [14, 23, 22, 21, 8],
                     [13, 12, 11, 10, 9]],
             size: 5,
-            endpoint: "http://{your machine's ip}:4001" // this is where we are connecting to with sockets
+            endpoint: "http://localhost:3000" // this is where we are connecting to with sockets
         }
     }
-    send() {
-        const socket = socketIOClient(this.state.endpoint)
-        
-        // this emits an event to the socket (your server) with an argument of 'red'
-        // you can make the argument any color you would like, or any kind of data you want to send.
-        
-        socket.emit('hello from front', 'red') 
-        // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
-    }
     render(){
-        const socket = socketIOClient(this.state.endpoint)
-        socket.on('change color', (color) => {
-            // setting the color of our button
-            document.body.style.backgroundColor = color
-          })
+        const socket = io(this.state.endpoint)
         return (
             // <div>
             //     <TileSection
@@ -37,7 +24,7 @@ class App extends Component{
             //     />
             // </div>
             <div style={{ textAlign: "center" }}>
-            <button onClick={() => this.send()}>Change Color</button>
+                <button onClick={() => console.log("button")}>Change Color</button>
           </div>
         )
     }
