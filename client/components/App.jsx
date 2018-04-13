aa472638643160b88ec69848d875813683b0cc75
aberlo <aberlo@student.42.fr>
2018-04-13 17:48:02 +0200

    client/socket working, need to link with server

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,30 +6,43 @@ import io from 'socket.io-client'
 class App extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            puzzle: [[1, 2, 3, 4, 5],
-                    [16, 17, 18, 19, 6],
-                    [15, 24, 0, 20, 7],
-                    [14, 23, 22, 21, 8],
-                    [13, 12, 11, 10, 9]],
-            size: 5
-        }
-    }
-    prevState(){
-        console.log('prev')
-    }
-    nextState(){
-        console.log('next')
-    }
-    go(){
-        console.log('go')
-    }
-    render(){
         var socket = io("http://localhost:3000")
         socket.on('connect', function(){
             console.log(socket)
             console.log("CONNECTED TO SERVER")            
         })
+        this.state = {
+            puzzle: [1, 2, 3, 4, 5, 16, 17, 18, 19, 6,15, 24, 0, 20, 7 ,14, 23, 22, 21, 8, 13, 12, 11, 10, 9],
+            size: 5,
+            socket
+        }
+    }
+    socketHandler(){
+        this.state.socket.on('State', function(data){
+            console.log('State')
+            this.setState({puzzle: data});
+        }.bind(this))
+
+        this.state.socket.on('Size', function(data){
+            console.log('Size')
+            this.setState({size: data});
+        }.bind(this))
+
+        this.state.socket.on('All', function(data){
+            console.log(data)
+        }.bind(this))
+    }
+    prevState(){
+        this.state.socket.emit('prevState')
+    }
+    nextState(){
+        this.state.socket.emit('nextState')
+    }
+    go(){
+        this.state.socket.emit('go')
+    }
+    render(){
+        this.socketHandler()
         return (
             <div>
                 <TileSection
@@ -42,9 +55,6 @@ class App extends Component{
                     {...this.state}
                 />
             </div>
-        //     <div style={{ textAlign: "center" }}>
-        //         <button onClick={() => console.log("button")}>Change Color</button>
-        //   </div>
         )
     }
 }
