29ad2b9537fe05a0afdf9d2555de5137a6814149
Louis-marie JOLY <ljoly@e2r10p1.42.fr>
2018-04-16 20:38:57 +0200

    client/wip: display solution state by state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,71 +1,69 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TileSection from './tiles/TileSection.jsx'
 import ButtonSection from './buttons/ButtonSection.jsx'
 import io from 'socket.io-client'
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props);
         var socket = io("http://localhost:3000")
-        socket.on('connect', function(){
-            console.log(socket)
-            console.log("CONNECTED TO SERVER")            
+        socket.on('connect', function () {
+            console.log("CONNECTED TO SERVER")
         })
         this.state = {
             index: 0,
             all: [],
-            // puzzle: [1, 2, 3, 4, 5, 16, 17, 18, 19, 6, 15, 24, 0, 20, 7 ,14, 23, 22, 21, 8, 13, 12, 11, 10, 9],
             puzzle: [],
             size: 0,
             socket
         }
     }
-    socketHandler(){
-        this.state.socket.on('state', function(data){
-            console.log('State', data)
+    socketHandler() {
+        this.state.socket.on('state', function (data) {
             data = JSON.parse(data)
-            this.setState({all: data});
-            this.setState({puzzle: this.state.all[0].Board, size: this.state.all[0].Size})
+            this.setState({ all: data });
+            this.setState({ puzzle: this.state.all[0].Board, size: this.state.all[0].Size })
         }.bind(this))
     }
-    prevState(){
-        console.log('PREV')
+    prevState() {
         if (this.state.index > 0) {
-            this.setState({index: this.state.index-1})
-            this.setState({puzzle: this.state.all[this.state.index].Board, size: this.state.all[this.state.index].Size})
+            this.state.index--
+            this.setState({ puzzle: this.state.all[this.state.index].Board, size: this.state.all[this.state.index].Size })
+            console.log('index', this.state.index)
         }
     }
-    nextState(){
-        console.log('NEXT')
-        if (this.state.index < this.state.all.length) {
-            this.setState({index: this.state.index+1})
-            this.setState({puzzle: this.state.all[this.state.index].Board, size: this.state.all[this.state.index].Size})
+    nextState() {
+        if (this.state.index < this.state.all.length - 1) {
+            this.state.index++
+            this.setState({ puzzle: this.state.all[this.state.index].Board, size: this.state.all[this.state.index].Size })
+            console.log('index', this.state.index)
         }
     }
-    go(){
-        var index = 0
-            while (index < this.state.all.length - 1) {
-                setTimeout(function(){
-                index++
-                console.log(index)
-                console.log(this.state.all[index].Board)
-                this.setState({puzzle: this.state.all[index].Board, size: this.state.all[index].Size})
-            }.bind(this), 3000);
+    go() {
+        var i = this.state.index
+        const self = this
+        while (i < self.state.all.length - 1) {
+            setInterval(function () {
+                setTimeout(() => self.setState({ puzzle: self.state.all[i].Board }), 1000)
+            }.bind(self), 1000)
+            i++
         }
+        this.state.index = i
+        console.log('index', this.state.index)
     }
-    componentDidMount(){
-        this.socketHandler()        
+    componentWillMount() {
+        this.socketHandler()
     }
-    render(){
+    render() {
         return (
             <div>
                 <TileSection
                     {...this.state}
                 />
                 <ButtonSection
-                    prevState = {this.prevState.bind(this)}
-                    nextState = {this.nextState.bind(this)}
-                    go = {this.go.bind(this)}
+                    prevState={this.prevState.bind(this)}
+                    nextState={this.nextState.bind(this)}
+                    go={this.go.bind(this)}
                     {...this.state}
                 />
             </div>
