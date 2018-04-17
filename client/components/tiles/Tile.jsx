f5560692472a9ad88d72393d75b1517948e0456b
Louis-marie JOLY <ljoly@e2r8p3.42.fr>
2018-04-17 15:46:21 +0200

    oui

--- client/components/tiles/Tile.jsx
+++ client/components/tiles/Tile.jsx
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-class Tile extends Component{
-    render(){
+class Tile extends Component {
+    render() {
         var r = (this.props.tile * this.props.size & 0xFF).toString(16);
         var g = ((this.props.tile * this.props.size >> 8) & 0xFF).toString(16);
         var b = ((this.props.tile * this.props.size >> 16) & 0xFF).toString(16);
-     
+
         r = ('0' + r).slice(-2);
         g = ('0' + g).slice(-2);
         b = ('0' + b).slice(-2);
-     
+
         const bgColor = "#" + r + g + b;
         if (this.props.tile != 0) {
             return (
-                <span className="tile" style={{backgroundColor: bgColor}}>
-                    {this.props.tile}  
+                <span className="tile" style={{ backgroundColor: bgColor }}>
+                    {this.props.tile}
                 </span>
             )
         } else {
             return (
-                <span className="tile" style={{backgroundColor: 'white'}}>&nbsp;</span>
+                <span className="tile" style={{ backgroundColor: 'white' }}>&nbsp;</span>
             )
         }
     }
